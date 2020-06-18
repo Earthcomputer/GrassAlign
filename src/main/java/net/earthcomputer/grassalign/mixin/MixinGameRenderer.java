0b9b8c2b490e6e16b36ecfd33cf0361332247545
Joe <burtonjae@hotmail.co.uk>
2020-06-18 18:05:24 +0100

    Beta render offset by 0.1

---
+++ src/main/java/net/earthcomputer/grassalign/mixin/MixinGameRenderer.java
@@ -0,0 +1,16 @@
+package net.earthcomputer.grassalign.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class MixinGameRenderer {
+    @Inject(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Camera;update(Lnet/minecraft/world/BlockView;Lnet/minecraft/entity/Entity;ZZF)V", shift = At.Shift.AFTER))
+    private void applyBetaOffset(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo ci) {
+        matrix.translate(0, 0, -0.1);
+    }
+}
